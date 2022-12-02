0e10162a8343e242d4fe3e239e982fbf4ab4a1c8
Zdenek <kolba.zdenek@gmail.com>
2022-12-02 06:58:55 +0100

    Day 2

--- main.py
+++ main.py
@@ -33,4 +33,49 @@ def day1():
     print("Top 3 calories sum: " + str(top1 + top2 + top3))
 
 
-day1()
+def day2():
+    file = open("inputs/day2_1.txt", "r")
+    total_part1 = 0
+    total_part2 = 0
+    for line in file.readlines():
+        opponent = line[0]
+        me = line[-2]
+        match opponent:
+            case "A":
+                match me:
+                    case "X":
+                        total_part1 += 1 + 3
+                        total_part2 += 3 + 0
+                    case "Y":
+                        total_part1 += 2 + 6
+                        total_part2 += 1 + 3
+                    case "Z":
+                        total_part1 += 3 + 0
+                        total_part2 += 2 + 6
+            case "B":
+                match me:
+                    case "X":
+                        total_part1 += 1 + 0
+                        total_part2 += 1 + 0
+                    case "Y":
+                        total_part1 += 2 + 3
+                        total_part2 += 2 + 3
+                    case "Z":
+                        total_part1 += 3 + 6
+                        total_part2 += 3 + 6
+            case "C":
+                match me:
+                    case "X":
+                        total_part1 += 1 + 6
+                        total_part2 += 2 + 0
+                    case "Y":
+                        total_part1 += 2 + 0
+                        total_part2 += 3 + 3
+                    case "Z":
+                        total_part1 += 3 + 3
+                        total_part2 += 1 + 6
+    print("Part 1 total score: " + str(total_part1))
+    print("Part 2 total score: " + str(total_part2))
+
+# day1()
+day2()
